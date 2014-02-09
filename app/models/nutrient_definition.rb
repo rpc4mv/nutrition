5ca9edb8141f20c2293250869f86c139e3c68957
Ruturaj Choudhari <rpc4mv@virginia.edu>
2014-02-09 01:21:43 -0800

    dont need to specify primary_key for nutrition data table..

--- app/models/nutrient_definition.rb
+++ app/models/nutrient_definition.rb
@@ -1,6 +1,7 @@
 class NutrientDefinition < ActiveRecord::Base
 
   has_many :nutrition_data, foreign_key: "nutr_no"
+  
   def self.primary_key
     "nutr_no"
   end
--- app/models/nutrition_datum.rb
+++ app/models/nutrition_datum.rb
@@ -3,7 +3,4 @@ class NutritionDatum < ActiveRecord::Base
   belongs_to :nutrient_definition, foreign_key: :nutr_no
   belongs_to :food_description, foreign_key: "ndb_no"
 
-  def self.primary_key
-    "ndb_no"
-  end
 end
